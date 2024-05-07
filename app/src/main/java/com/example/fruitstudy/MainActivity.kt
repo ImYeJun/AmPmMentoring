70345544199315909739964dc971c8a99d5a3992
ImNotYejun <yejun6210@gmail.com>
2024-05-07 18:08:46 +0900

    5/7 first commit

--- app/src/main/java/com/example/fruitstudy/MainActivity.kt
+++ app/src/main/java/com/example/fruitstudy/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.fruitstudy
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.Toast
@@ -20,26 +21,33 @@ class MainActivity : AppCompatActivity() {
         val image6 = findViewById<ImageView>(R.id.fruit_image_6)
         val image7 = findViewById<ImageView>(R.id.fruit_image_7)
 
+        //사진을 클릭하면 Toast로 메세지 띄우기
         image1.setOnClickListener{
-            Toast.makeText(this, "1번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "1번 사진 클릭",Toast.LENGTH_SHORT).show()
+
+            // 2.화면이 클릭되면 다른 화면으로 넘어감
+            // 구글링 : "안드로이드 스튜디오 화면 전환 kotlin"
+
+            var intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
         }
         image2.setOnClickListener{
-            Toast.makeText(this, "2번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "2번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
         image3.setOnClickListener{
-            Toast.makeText(this, "3번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "3번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
         image4.setOnClickListener{
-            Toast.makeText(this, "4번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "4번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
         image5.setOnClickListener{
-            Toast.makeText(this, "5번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "5번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
         image6.setOnClickListener{
-            Toast.makeText(this, "6번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "6번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
         image7.setOnClickListener{
-            Toast.makeText(this, "7번 사진 클릭",Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "7번 사진 클릭",Toast.LENGTH_SHORT).show()
         }
     }
 
